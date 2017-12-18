ccb3378e5b4d51b29a1ffb059a71bd285ea90162
devmohammedothman <mohammed.fci86@gmail.com>
2017-12-18 18:40:14 +0200

    commit on branch level with egit

--- src/main/java/com/channelit/crudangularspringboot/CrudangularspringbootApplication.java
+++ src/main/java/com/channelit/crudangularspringboot/CrudangularspringbootApplication.java
@@ -8,7 +8,11 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = "com.channelit.crudangularspringboot.entity")
 public class CrudangularspringbootApplication {
 
+	//commit on branch level
 	public static void main(String[] args) {
+		System.out.println("start application");
 		SpringApplication.run(CrudangularspringbootApplication.class, args);
+		
+		
 	}
 }
